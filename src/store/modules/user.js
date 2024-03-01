9a43a99a00781d1f4f99f2f1ce3c455127084ed1
TofuO1d <chencb31415926@163.com>
2024-03-01 16:19:36 +0800

    完成登录检验和token存储

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,9 +1,20 @@
+import { loginAPI } from '@/api/user'
+import { setToken, getToken } from '@/utils/auth'
 export default {
+  namespaced: true,
   state: {
-
+    token: getToken() || ''
   },
   mutations: {
-
+    setToken (state, newToken) {
+      state.token = newToken
+      setToken(newToken)
+    }
   },
-  actions: {}
+  actions: {
+    async loginAction (store, data) {
+      const res = await loginAPI(data)
+      store.commit('setToken', res.data.token)
+    }
+  }
 }
