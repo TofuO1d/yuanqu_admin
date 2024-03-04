f98c5bd1d263dd5424433467dbe13080d93ca0a5
TofuO1d <chencb31415926@163.com>
2024-03-04 18:28:26 +0800

    车辆计费功能完善

--- src/views/Car/CarRule/components/AddRule.vue
+++ src/views/Car/CarRule/components/AddRule.vue
@@ -13,7 +13,6 @@
         :model="addForm"
         :rules="addFormRules"
         label-position="top"
-        require
       >
         <el-form-item label="计费规则编号" prop="ruleNumber">
           <el-input v-model="addForm.ruleNumber" />
@@ -21,13 +20,7 @@
         <el-form-item label="计费规则名称" prop="ruleName">
           <el-input v-model="addForm.ruleName" />
         </el-form-item>
-        <el-form-item label="计费方式" prop="chargeType">
-          <el-radio-group v-model="addForm.chargeType" size="small">
-            <el-radio label="duration" border>时长收费</el-radio>
-            <el-radio label="turn" border>按次收费</el-radio>
-            <el-radio label="partition" border>分段收费</el-radio>
-          </el-radio-group>
-        </el-form-item>
+
         <div class="flex-container">
           <el-form-item label="免费时长">
             <el-input v-model="addForm.freeDuration" />
@@ -36,40 +29,67 @@
             <el-input v-model="addForm.chargeCeiling" />
           </el-form-item>
         </div>
-        <el-form-item label="计费规则" :prop="feeRuleProp">
+        <el-form-item label="计费方式" prop="chargeType">
+          <el-radio-group v-model="addForm.chargeType" size="small">
+            <el-radio label="duration" border>时长收费</el-radio>
+            <el-radio label="turn" border>按次收费</el-radio>
+            <el-radio label="partition" border>分段收费</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="计费规则">
           <!-- 按时长收费区域 -->
           <div v-if="addForm.chargeType === 'duration'" class="duration">
             每
-            <el-input v-model="addForm.durationTime" class="input-box" />
+            <el-form-item prop="durationTime">
+              <el-input v-model="addForm.durationTime" class="input-box" />
+            </el-form-item>
             小时
-            <el-input v-model="addForm.durationPrice" class="input-box" />
+            <el-form-item prop="durationPrice">
+              <el-input v-model="addForm.durationPrice" class="input-box" />
+            </el-form-item>
             元
           </div>
           <!-- 按次收费区域 -->
           <div v-if="addForm.chargeType === 'turn'" class="turn">
             每次
-            <el-input v-model="addForm.turnPrice" class="input-box" /> 元
+            <el-form-item prop="turnPrice">
+              <el-input v-model="addForm.turnPrice" class="input-box" />
+            </el-form-item>
+
+            元
           </div>
           <!-- 按分段收费区域 -->
           <div v-if="addForm.chargeType === 'partition'" class="partition">
             <div class="item">
-              <el-input
-                v-model="addForm.partitionFrameTime"
-                class="input-box"
-              />小时内,每小时收费<el-input
-                v-model="addForm.partitionFramePrice"
-                class="input-box"
-              />
+              <el-form-item prop="partitionFrameTime">
+                <el-input
+                  v-model="addForm.partitionFrameTime"
+                  class="input-box"
+                />
+              </el-form-item>
+              <el-form-item prop="partitionFramePrice">
+                小时内,每小时收费<el-input
+                  v-model="addForm.partitionFramePrice"
+                  class="input-box"
+                />
+              </el-form-item>
               元
             </div>
             <div class="item">
-              每增加<el-input
-                v-model="addForm.partitionIncreaseTime"
-                class="input-box"
-              />小时,增加<el-input
-                v-model="addForm.partitionIncreasePrice"
-                class="input-box"
-              />
+              每增加
+              <el-form-item prop="partitionIncreaseTime">
+                <el-input
+                  v-model="addForm.partitionIncreaseTime"
+                  class="input-box"
+                />
+              </el-form-item>
+              小时,增加
+              <el-form-item prop="partitionIncreasePrice">
+                <el-input
+                  v-model="addForm.partitionIncreasePrice"
+                  class="input-box"
+                />
+              </el-form-item>
               元
             </div>
           </div>
@@ -140,13 +160,61 @@ export default {
             message: '请选择收费类型',
             trigger: 'blur'
           }
+        ],
+        durationTime: [
+          {
+            required: true,
+            message: '请输入时间',
+            trigger: 'blur'
+          }
+        ],
+        durationPrice: [
+          {
+            required: true,
+            message: '请输入金额',
+            trigger: 'blur'
+          }
+        ],
+        turnPrice: [
+          {
+            required: true,
+            message: '请输入每次费用',
+            trigger: 'blur'
+          }
+        ],
+        partitionFrameTime: [
+          {
+            required: true,
+            message: '请输入段内时间',
+            trigger: 'blur'
+          }
+        ],
+        partitionFramePrice: [
+          {
+            required: true,
+            message: '请输入段内费用',
+            trigger: 'blur'
+          }
+        ],
+        partitionIncreaseTime: [
+          {
+            required: true,
+            message: '请输入超出时间',
+            trigger: 'blur'
+          }
+        ],
+        partitionIncreasePrice: [
+          {
+            required: true,
+            message: '请输入超出费用',
+            trigger: 'blur'
+          }
         ]
       } // 计费规则校验规则对
     }
   },
   watch: {
     'addForm.chargeType' (val) {
-      console.log(111)
       this.$refs.addForm.clearValidate([
         'turnPrice',
         'durationTime',
@@ -156,16 +224,6 @@ export default {
         'partitionIncreaseTime',
         'partitionIncreasePrice'
       ])
-      if (val === 'duration') {
-        this.feeRuleProp = 'durationTime'
-      }
-      if (val === 'turn') {
-        this.feeRuleProp = 'turnPrice'
-      }
-      if (val === 'partition') {
-        this.feeRuleProp = 'partitionFrameTime'
-      }
-      console.log(this.feeRuleProp)
     },
     dialogVisible (val) {
       if (val === true && this.editId) {
